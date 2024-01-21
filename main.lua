703e8f625b1084cf9bef6d2405f54168ae477d12
Nate <npzaldivar@gmail.com>
2024-01-20 22:27:34 -0700

    Score now only increases when target is pressed

--- main.lua
+++ main.lua
@@ -28,9 +28,9 @@ function love.update(dt)
 end
 
 function love.draw()
+    -- Set the 'Score' card.
     love.graphics.setColor((239/255),(208/255),(158/255))
     love.graphics.rectangle("fill",score_x_coordinate,0,score_length,score_height)
-
     love.graphics.setColor((144/255),(169/255),(183/255))
     local textWidth = gameFont:getWidth("Score")
     love.graphics.print("SCORE",(score_x_coordinate+(score_length/2)-(score_length-textWidth)/1.3),0)
@@ -38,19 +38,26 @@ function love.draw()
     love.graphics.setColor((144/255),(169/255),(183/255))
     love.graphics.circle("fill",target.x,target.y,target.radius)
 
-    
+    -- Update 'Score' in real time.
     if type(score) == "number" and score >= 10 and score <= 99 then
         font_adjustment = 30
     elseif type(score) == "number" and score >= 10 and score <= 999 then
         font_adjustment = 45
     end
-
     love.graphics.setFont(gameFont)
     love.graphics.print(score,(score_x_coordinate+(score_length/2)-font_adjustment),50)
 end
 
 function love.mousepressed( x, y, button, istouch, presses )
+    -- Increase score every time button is pressed
     if button == 1 then
-        score = score + 1
+        local mouseToTarget = distanceBetween(x, y, target.x, target.y)
+        if mouseToTarget < target.radius then
+            score = score + 1
+        end
     end
+end
+
+function distanceBetween(x1, y1, x2, y2)
+    return math.sqrt( (x2-x1)^2 + (y2-y1)^2 )
 end
